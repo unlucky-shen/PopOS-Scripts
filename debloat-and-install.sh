781faf9c03ed6584f3ce80a85cff00b21e86b8b7
unlucky-shen <taufiqhaikal002@gmail.com>
2025-11-26 21:58:17 +0800

    added neovim and steam

--- debloat-and-install.sh
+++ debloat-and-install.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Debloat
-sudo apt purge gnome-calculator gnome-calendar gnome-contacts gucharmap evince simple-scan nano libreoffice* vim* firefox* popsicle* pop-shop* gnome-weather seahorse geary* gedit -y
+sudo apt purge gnome-calculator gnome-calendar gnome-contacts gucharmap evince simple-scan nano* libreoffice* vim* firefox* popsicle* pop-shop* gnome-weather seahorse geary* gedit -y
 
 # Essential Packages 
-sudo apt install wget curl flatpak htop openssh-client openssh-server fzf ripgrep fd-find zathura tmux gcc gfortran python3 r-base mesa-utils vulkan-tools libvulkan1 vulkan-validationlayers mesa-vulkan-drivers -y
+sudo apt install wget curl neovim flatpak htop openssh-client openssh-server fzf ripgrep fd-find zathura tmux gcc gfortran python3 r-base mesa-utils vulkan-tools libvulkan1 vulkan-validationlayers mesa-vulkan-drivers steam -y
