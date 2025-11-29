d5685b64033c70dc6210c2b7d57970003640d46c
Taufiq Haikal <taufiqhaikal002@gmail.com>
2025-11-29 04:41:19 +0000

    Comment out installation commands in devtools.sh
    Comment out installation commands for NVM, Rust, and Typst CLI.

--- devtools.sh
+++ devtools.sh
@@ -4,12 +4,15 @@ set -e
 
 # NVM (Nodejs-LTS)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-nvm install --lts
-npm install -g tree-sitter-cli
+# nvm install --lts
+# npm install -g tree-sitter-cli
 
 # Rust (Rustup)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-cargo install --locked typst-cli
+# cargo install --locked typst-cli
 
 # uv-astral (Python pip replacement)
 curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Starship prompt
+curl -sS https://starship.rs/install.sh | sh
