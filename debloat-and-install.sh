f7c2db22416d01032a117c87aa99a8b7fd135fbf
Taufiq Haikal <taufiqhaikal002@gmail.com>
2025-11-29 05:12:48 +0000

    Remove nano and libreoffice from installation script

--- debloat-and-install.sh
+++ debloat-and-install.sh
@@ -6,4 +6,4 @@ set -e
 sudo apt purge gnome-calculator gnome-calendar gnome-contacts gucharmap evince simple-scan nano* libreoffice* vim* firefox* popsicle* pop-shop* gnome-weather seahorse geary* gedit -y
 
 # Essential Packages 
-sudo apt install wget curl xclip unzip p7zip-full libarchive-tools neovim flatpak htop openssh-client openssh-server fzf ripgrep fd-find zathura tmux gcc gfortran python3 r-base mesa-utils vulkan-tools libvulkan1 vulkan-validationlayers mesa-vulkan-drivers steam -y
+sudo apt install wget curl xclip unzip p7zip-full libarchive-tools flatpak htop openssh-client openssh-server fzf ripgrep fd-find zathura tmux gcc gfortran python3 r-base mesa-utils vulkan-tools libvulkan1 vulkan-validationlayers mesa-vulkan-drivers steam -y
