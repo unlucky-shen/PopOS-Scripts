54107c8860f8148699033f2d7c1f09a29b2f8f6c
Taufiq Haikal <taufiqhaikal002@gmail.com>
2025-11-29 04:44:56 +0000

    Update devtools.sh

--- devtools.sh
+++ devtools.sh
@@ -9,10 +9,20 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 # Rust (Rustup)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-# cargo install --locked typst-cli
 
 # uv-astral (Python pip replacement)
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # Starship prompt
 curl -sS https://starship.rs/install.sh | sh
+
+# Typst
+wget -qO typst.tar.xz https://github.com/typst/typst/releases/latest/download/typst-x86_64-unknown-linux-musl.tar.xz
+
+sudo tar xf typst.tar.xz --strip-components=1 -C /usr/local/bin typst-x86_64-unknown-linux-musl/typst
+
+typst --version
+
+rm -rf typst.tar.xz
+
+
