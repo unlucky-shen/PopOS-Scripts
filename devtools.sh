4ac75d3602d455cfdc18dfae49af5f8dcb55b824
Taufiq Haikal <taufiqhaikal002@gmail.com>
2025-11-29 04:52:06 +0000

    Update devtools.sh

--- devtools.sh
+++ devtools.sh
@@ -4,8 +4,6 @@ set -e
 
 # NVM (Nodejs-LTS)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-# nvm install --lts
-# npm install -g tree-sitter-cli
 
 # Rust (Rustup)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
